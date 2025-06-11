bb9b993358fb9bcbad31822136e04aa4fcba2b47
Alwithyou <ahnafalfarezsutrisno@gmail.com>
2025-06-11 11:50:21 +0700

    test

--- resources/views/user/profil.blade.php
+++ resources/views/user/profil.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body class="bg-gray-100 text-gray-800">
     <div class="container mx-auto mt-10">
-        <h1 class="text-3xl font-bold mb-4">Dashboard Admin</h1>
+        <h1 class="text-3xl font-bold mb-4">Dashboard</h1>
     </div>
 </body>
 </html>
